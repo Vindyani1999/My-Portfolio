ca6346fdb9a2587c50f9527b781c60ba3c9ec44e
Vindyani1999 <k.a.c.h.vindyani@gmail.com>
2024-05-25 18:49:49 +0530

    Updated projects

--- src/components/projects/ProjectLaypot.jsx
+++ src/components/projects/ProjectLaypot.jsx
@@ -8,22 +8,27 @@ const item = {
   show: { opacity: 1, y: 0 },
 };
 
-
 const ProjectLink = motion(Link);
 
-const ProjectLayput = ({ name, description, date, demoLink, image }) => {
+const ProjectLaypot = ({ name, description, date, demoLink, image, type }) => {
   return (
     <ProjectLink
-    variants={item} href={demoLink} target="_blank" className="text-sm md:text-base flex  items-center justify-between w-full relative rounded-lg overflow-hidden p-4 md:p-6 custom-bg">
-      <div className="flex items-center justify-center space-x-2">
-        <img src={image} alt={name} className=" w-12 h-12 lg:w-20 lg:h-20 rounded-md opacity-80 mx-2" /> {/* Adjust the width and height as needed */}
-        <h2 className="text-foreground">{name}</h2>
-        <p className="text-muted hidden sm:inline-block">{description}</p>
+      variants={item}
+      href={demoLink}
+      target="_blank"
+      className="text-sm md:text-base flex flex-col items-center justify-between w-full relative rounded-lg overflow-hidden p-4 md:p-3 custom-bg"
+    >
+      <div className="flex flex-col items-center justify-center space-y-2">
+      <img src={image} alt={name} className="w-full h-32 lg:h-35 rounded-md opacity-80 mx-2 object-cover" />
+        <h2 className="text-foreground items-center">{name}</h2>
+        <p className="text-muted hidden items-center sm:inline-block">{description}</p>
       </div>
-      <div className="self-end flex-1 mx-2 mb-1 bg-transparent  border-muted" />
-      <p className="text-muted hidden sm:inline-block xs:text-foreground">{new Date(date).toDateString()}</p>
+      <div className="self-end flex-1 mx-2 mb-1 bg-transparent border-muted" />
+      <br></br>
+      <p className="text-muted hidden sm:inline-block xs:text-foreground text-xs">{type}</p>
+
     </ProjectLink>
   );
 };
 
-export default ProjectLayput;
+export default ProjectLaypot;
--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -14,17 +14,19 @@ const container = {
   },
 };
 
-const ProjectList = ({projects}) => {
+const ProjectList = ({ projects }) => {
   return (
-    <motion.div 
-    variants={container}
+    <motion.div
+      variants={container}
       initial="hidden"
-      animate="show" className="w-full max-w-auto  xl:max-w-4xl px-4 mx-auto lg:px-16 space-y-6 md:space-y-8 flex flex-col items-center">
-        {projects.map((project,index)=>{
-            return <ProjectLaypot key={index} {...project}/>
-        })}
+      animate="show"
+      className="w-full max-w-auto xl:max-w-4xl px-4 mx-auto lg:px-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6"
+    >
+      {projects.map((project, index) => (
+        <ProjectLaypot key={index} {...project} />
+      ))}
     </motion.div>
-  )
+  );
 }
 
-export default ProjectList
+export default ProjectList;
